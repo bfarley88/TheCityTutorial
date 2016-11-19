6c40481f9a10acbfdd2b46550b35b7198537bab0
Brandon Farley <bfarley88@gmail.com>
2016-11-19 12:48:51 -0800

    Add files via upload
    tutorial

---
+++ Unity Scripts/Car1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car1Controller : MonoBehaviour {
+
+	float speedForce = 32f;
+	float torqueForce = 1.7f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		// check for button up down here
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if(Input.GetButton("AButton")) {
+			rb.AddForce( transform.up * speedForce );
+		}
+
+		rb.AddTorque( Input.GetAxis("LeftJoystickHorizontal") * torqueForce );
+	}
+}
---
+++ Unity Scripts/Drone2dController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone2dController : MonoBehaviour {
+
+	float speedForce = 13f;
+	float torqueForce = 6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void Update() {
+		// check for button up down here
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		if(Input.GetButton("AButton")) {
+			rb.AddForce( transform.up * speedForce );
+		}
+
+		rb.AddTorque( Input.GetAxis("LeftJoystickHorizontal") * torqueForce );
+	}
+}
